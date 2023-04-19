9d6573b90bfd6b003949a41c99eb0f81f279a881
Angela C <57610235+aycode101@users.noreply.github.com>
2023-04-19 13:20:10 -0500

    Added CometCash to sections and fixed Tobor getting stuck

--- Assets/Scripts/Tobor/ToborScript.cs
+++ Assets/Scripts/Tobor/ToborScript.cs
@@ -7,25 +7,38 @@ public class ToborScript : MonoBehaviour
     private bool right;
     public float toborSpeed = 70;
     //private bool move = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        if ((this.gameObject.transform.position.x - LevelBoundary.leftSide) < (LevelBoundary.rightSide - this.gameObject.transform.position.x ))
+        if ((this.gameObject.transform.position.x - LevelBoundary.leftSide) < (LevelBoundary.rightSide - this.gameObject.transform.position.x))
         {
             right = true;
         }
         else
+        {
             right = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!(transform.position.x > (LevelBoundary.leftSide)) || !(transform.position.x < LevelBoundary.rightSide))
+        //if(!(transform.position.x > LevelBoundary.leftSide) || !(transform.position.x < LevelBoundary.rightSide))
+        //{
+        //    right = !right;
+        //}
+
+        if (transform.position.x < LevelBoundary.leftSide)
         {
-            right = !right;
+            right = true;
         }
-        if(right){
+        if (transform.position.x > LevelBoundary.rightSide)
+        {
+            right = false;
+        }
+
+        if (right){
             transform.Translate(Vector3.down * Time.deltaTime * toborSpeed);
         }
         else{
