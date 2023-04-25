d052c512d2174d7b61763db487f09755f0c59a43
Angela C <57610235+aycode101@users.noreply.github.com>
2023-04-25 04:04:29 -0500

    Changed water sprout script with coroutine

--- Assets/Scripts/WaterNozzle/WaterNozzleScale.cs
+++ Assets/Scripts/WaterNozzle/WaterNozzleScale.cs
@@ -5,34 +5,63 @@ using UnityEngine;
 public class WaterNozzleScale : MonoBehaviour
 {
     private Vector3 scaleChange, positionChange;
-    public float speed = 1;
+    public float speed = 10;
+    bool goingUp = false;
 
-    // Start is called before the first frame update
     void Awake()
     {
         scaleChange = new Vector3(0, -0.01f, 0);
         positionChange = new Vector3(0, -0.005f, 0);
 
+        StartCoroutine(waterMove());
     }
 
-    // Update is called once per frame
+    private IEnumerator waterMove()
+    {
+        while (true)
+        {
+            if (transform.localPosition.y <= -7.85f)
+            {
+                goingUp = true;
+                yield return new WaitForSeconds(2);
+            }
+            if (transform.localPosition.y >= -3.2f)
+            {
+                goingUp = false;
+            }
+
+            if (goingUp)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * speed);
+            }
+            else
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * speed);
+            }
+
+            yield return null;
+        }
+    }
+
     void Update()
     {
-        this.gameObject.transform.localScale += scaleChange;
-        this.gameObject.transform.position += positionChange;
+        //  this.gameObject.transform.localScale += scaleChange;
+        //  this.gameObject.transform.position += positionChange;
 
-        // changes scale of water obj to constantly go up and down
-        // TODO: have it to change scale at random times + slower speeds too?
-      //StartCoroutine(TimedScaling());
-        if (this.gameObject.transform.localScale.y < 0.1f || this.gameObject.transform.localScale.y > 1.0f)
-        {
-            scaleChange = -scaleChange;
-            positionChange = -positionChange;
-          //StartCoroutine(TimedScaling());
-        }
+        //  // changes scale of water obj to constantly go up and down
+        //  // TODO: have it to change scale at random times + slower speeds too?
+        ////StartCoroutine(TimedScaling());
+        //  if (this.gameObject.transform.localScale.y < 0.1f || this.gameObject.transform.localScale.y > 1.0f)
+        //  {
+        //      scaleChange = -scaleChange;
+        //      positionChange = -positionChange;
+        //    //StartCoroutine(TimedScaling());
+        //  }
+
     }
     IEnumerator TimedScaling() 
     {
         yield return new WaitForSeconds(10);
     }
+
 }
