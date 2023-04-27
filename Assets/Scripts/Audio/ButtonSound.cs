4ae9017030824e0e6f27e82257539295d471d67f
Haniyyah Hamid <hamidhaniyyahb@gmail.com>
2023-04-27 13:24:31 -0500

    added static sound to PlayGame button on mainmenu + moved files to folders

---
+++ Assets/Scripts/Audio/ButtonSound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.PlayOneShot(clip);
+    }
+}
